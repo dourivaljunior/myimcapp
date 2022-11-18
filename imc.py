9ca78e97b937252eacaddd1e63097b443ca2102d
Prof. Dourival Júnior <100372396+dourivaljunior@users.noreply.github.com>
2022-11-17 23:11:45 -0300

    Add files via upload

---
+++ imc.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request
+app = Flask(__name__)
+@app.route("/imc", methods=["GET","POST"])
+def imc():
+    imc=0
+    if request.method=='POST' and 'peso' in request.form and 'altura' in request.form:
+        peso=float(request.form.get('peso'))
+        altura = float(request.form.get('altura'))
+        imc=round(peso/((altura/100)**2),1)
+    return render_template("imc.html",imc=imc)
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
